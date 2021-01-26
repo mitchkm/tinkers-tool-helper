490b974db72bbe679e3c56d2bd0432393f52b1c1
Jace7Valentine <mitchkm12@gmail.com>
2021-01-26 12:45:38 -0800

    adds veutify

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  chainWebpack: config => {
-      config
-          .plugin('html')
-          .tap(args => {
-              args[0].title = "Tinker's Helper";
-              return args;
-          })
-  }
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
